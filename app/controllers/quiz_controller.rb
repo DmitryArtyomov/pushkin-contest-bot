3e14c7d591bb695fbb41bd3ff0d06c48e8852639
Dmitry Artyomov <artyomovd@yandex.ru>
2017-01-21 01:22:25 +0300

    Realize level 2

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -12,7 +12,27 @@ class QuizController < ApplicationController
 
     case level
     when 1
-      answer = $level_1[question.remove_punctuation]
+      answer = $level_1[question.remove_all_punctuation]
+    when 2
+      search_result = []
+      question.remove_word.remove_punctuation_but_spaces.split(' ').each do |question_word|
+        search_result.push($level_2[question_word])
+      end
+      search_result.compact!
+      resulting_indexes = nil
+      search_result.each do |s_result|
+        if resulting_indexes.nil?
+          resulting_indexes = s_result
+        else
+          resulting_indexes &= s_result
+        end
+      end
+
+      unless resulting_indexes.nil?
+        regex = question.sub('%WORD%', 'MySwapWord').remove_punctuation_but_spaces.squeeze(' ').sub('MySwapWord','([[:word:]]+)')
+        poem_line = $poems[resulting_indexes[0][0]][1][resulting_indexes[0][1]].remove_punctuation_but_spaces.squeeze(' ')
+        answer = poem_line[/#{regex}/, 1]
+      end
     end
 
     send_answer(answer, task_id)
--- config/initializers/poems_loader.rb
+++ config/initializers/poems_loader.rb
@@ -1,6 +1,14 @@
 class String
-  def remove_punctuation
+  def remove_all_punctuation
     self.gsub(/[^[:word:]]/, '').gsub('_','')
+  end
+
+  def remove_punctuation_but_spaces
+    self.gsub(/[^[:word:]\s]/, '').gsub('_','')
+  end
+
+  def remove_word
+    self.gsub('%WORD%', '')
   end
 end
 
@@ -10,6 +18,18 @@ $level_1 = {}
 
 $poems.each do |poem|
   poem[1].each do |line|
-    $level_1[line.remove_punctuation] = poem[0]
+    $level_1[line.remove_all_punctuation] = poem[0]
+  end
+end
+
+
+$level_2 = {}
+
+$poems.each_with_index do |poem, poem_index|
+  poem[1].each_with_index do |line, line_index|
+    line.remove_punctuation_but_spaces.split(' ').each do |word|
+      $level_2[word] = [] if $level_2[word].nil?
+      $level_2[word].push([poem_index, line_index])
+    end
   end
 end
