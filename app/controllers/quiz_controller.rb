7098acbc5594b64e569ce5c7b66536563ba37ef0
Dmitry Artyomov <artyomovd@yandex.ru>
2017-01-22 14:25:06 +0300

    implement level 3,4

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -14,29 +14,54 @@ class QuizController < ApplicationController
     when 1
       answer = $level_1[question.remove_all_punctuation]
     when 2
-      search_result = []
-      question.remove_word.remove_punctuation_but_spaces.split(' ').each do |question_word|
-        search_result.push($level_2[question_word])
-      end
-      search_result.compact!
+      resulting_indexes = find_poem_line(question)
+      answer = find_swapped_word(question, resulting_indexes)      
+    when 3, 4
       resulting_indexes = nil
-      search_result.each do |s_result|
+      answers = []
+      question.split("\n").each do |question_line|        
         if resulting_indexes.nil?
-          resulting_indexes = s_result
+          resulting_indexes = find_poem_line(question_line)
+          answers.push(find_swapped_word(question_line, resulting_indexes))
         else
-          resulting_indexes &= s_result
+          resulting_indexes = [[resulting_indexes[0][0],resulting_indexes[0][1] + 1]]
+          answers.push(find_swapped_word(question_line, resulting_indexes))
         end
       end
-
-      unless resulting_indexes.nil?
-        regex = question.sub('%WORD%', 'MySwapWord').remove_punctuation_but_spaces.squeeze(' ').sub('MySwapWord','([[:word:]]+)')
-        poem_line = $poems[resulting_indexes[0][0]][1][resulting_indexes[0][1]].remove_punctuation_but_spaces.squeeze(' ')
-        answer = poem_line[/#{regex}/, 1]
-      end
+      answer = answers.join(',')
     end
 
     send_answer(answer, task_id)
     SaveTaskWorker.perform_async(question, task_id, level, time_received, answer)
+  end  
+  
+  def find_swapped_word(question, indexes)
+    answer = nil    
+    unless indexes.nil?
+      unless $poems[indexes[0][0]][1][indexes[0][1]].nil?
+        regex = question.sub('%WORD%', 'MySwapWord').remove_punctuation_but_spaces.squeeze(' ').strip.sub('MySwapWord','([[:word:]]+)')
+        poem_line = $poems[indexes[0][0]][1][indexes[0][1]].remove_punctuation_but_spaces.strip.squeeze(' ')
+        answer = poem_line[/#{regex}/, 1]
+      end
+    end
+    answer
+  end
+
+  def find_poem_line(question)
+    search_result = []
+    question.remove_word.remove_punctuation_but_spaces.split(' ').each do |question_word|
+      search_result.push($level_2[question_word])
+    end
+    search_result.compact!
+    resulting_indexes = nil
+    search_result.each do |s_result|
+      if resulting_indexes.nil?
+        resulting_indexes = s_result
+      else
+        resulting_indexes &= s_result
+      end
+    end
+    resulting_indexes
   end
 
   def send_answer(answer, task_id)
